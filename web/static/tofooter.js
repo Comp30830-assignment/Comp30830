dec5c2363f0f54951a937847f7b8cf9dafebf0d9
JuiceCoffee <niuzhaozhong@gmail.com>
2023-04-16 07:30:57 +0100

    website

--- web/static/tofooter.js
+++
@@ -1,6 +0,0 @@
-var contactBtn = document.getElementById('contact-btn');
-var contactSection = document.getElementById('contact');
-
-contactBtn.addEventListener('click', function() {
-  contactSection.scrollIntoView({ behavior: 'smooth' });
-});
